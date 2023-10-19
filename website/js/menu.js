b85c49b0ad76932b0e62ad0c7224df8e619c6971
berlinetta812 <66923307+berlinetta812@users.noreply.github.com>
2023-10-19 22:39:06 +0300

    Delete website directory

--- website/js/menu.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.menu_burger').click(function() {
-        $('.menu_burger').toggleClass('open-menu');
-        $('.burger_nav').toggleClass('open-menu')
-    });
-});
